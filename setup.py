0ed574fe3d69eb6e9050f86d5a22e002d0ab1560
Ruslan Khamidullin <731ruslan00@mail.ru>
2022-05-16 21:09:10 +0300

    Init commit

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 print(requirements)
 setup(
-    name='myn_utils',
+    name='auth_utils',
     version='1.0.0',
     description='utils service',
+    author='ruslan',
+    author_email='731ruslan00@mail.ru',
     packages=find_packages(exclude=['tests*']),
     python_requires='>=3.7',
     install_requires=requirements,
